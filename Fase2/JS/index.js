9f1d52b8f6e3bc9d4e7ed73fea2f443e1f05247a
Catarina <a93259@alunos.uminho.pt>
2022-01-11 18:57:39 +0000

    about us done

--- Fase2/JS/index.js
+++ Fase2/JS/index.js
@@ -2,8 +2,14 @@
 let usernameEl = document.getElementById("username")
 
 function login(){
+    window.location.replace("http://127.0.0.1:5500/HTML/frontPage.html")
+}
 
+function toAboutUs(){
+    window.location.replace("http://127.0.0.1:5500/HTML/aboutUs.html")
+}
 
+function toFrontPage(){
     window.location.replace("http://127.0.0.1:5500/HTML/frontPage.html")
 
 }
