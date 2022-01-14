e35b851ebb7ea58b852107636df80e2a46a6ec1a
Catarina <a93259@alunos.uminho.pt>
2022-01-14 17:31:20 +0000

    + paginas

--- Fase2/Front-End/JS/index.js
+++ Fase2/Front-End/JS/index.js
@@ -1,8 +1,99 @@
 
-let usernameEl = document.getElementById("username")
+
+
+const data = {
+    name:"mariana"
+};
+
+fetch('http://127.0.0.1:8080/api/v1/person', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+    })
+    .then((response) =>{
+
+    const test = JSON.parse(response);
+
+    console.log(test[0].id);
+    })
+    
+    .then((data) => {
+    // console.log('Success:', data);
+    })
+
+    .catch((error) => {
+    console.error('Error:', error);
+});
+
+
+
+// const Http = new XMLHttpRequest();
+// const url = 'http://127.0.0.1:8080/api/v1/person';
+// Http.open("GET", url);
+// Http.send();
+
+
+// Http.onreadystatechange = (e) => {
+
+//     const test = JSON.parse(Http.responseText);
+//     const x = test[0].id;
+
+//     console.log(x);
+//     usernameEl.textContent = Http.responseText;
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
 
 function login(){
+
+    // var obj = new Object();
+    // obj.username = document.getElementById("username").value;
+    // obj.password = document.getElementById("password").value;
+
+    
+    // fetch('http://127.0.0.1:8080/api/v1/person', {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify(obj),
+    //     })
+    //     .then((data) => {
+    //     console.log('Success:', data);
+    //     })
+    //     .catch((error) => {
+    //     console.error('Error:', error);
+    // });
+
+
     window.location.replace("http://127.0.0.1:5500/HTML/frontPage.html")
+    
+}
+
+function toSignIn(){
+    window.location.replace("http://127.0.0.1:5500/HTML/signIn.html")
+}
+
+function toOwnerPage(){
+    window.location.replace("http://127.0.0.1:5500/HTML/ownerPage.html")
+
+}
+
+function toSignInOwner(){
+    window.location.replace("http://127.0.0.1:5500/HTML/signInOwner.html")
 }
 
 function toAboutUs(){
@@ -14,7 +105,7 @@ function toFrontPage(){
     window.location.reload();
 }
 
-function logOut(){
+function toIndex(){
     window.location.replace("http://127.0.0.1:5500/HTML/index.html")
 }
 
