36af7f221fd39217d7fe969c227b7c17d90fbbc7
Catarina <a93259@alunos.uminho.pt>
2022-01-15 18:15:17 +0000

    some thing

--- Fase2/Front-End/JS/index.js
+++ Fase2/Front-End/JS/index.js
@@ -217,7 +217,7 @@ function signInUser(){
     
     if( password1 === password2 ){
 
-        var data = {
+        var data1 = {
             nif : document.getElementById("nif").value,
             nome : document.getElementById("name").value,
             email : document.getElementById("email").value,
@@ -232,7 +232,7 @@ function signInUser(){
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify(data),
+            body: JSON.stringify(data1),
             })
             .then((data) => {
             console.log('Success:', data);
@@ -256,7 +256,7 @@ function signInOwner(){
     
     if( password1 === password2 ){
 
-        var data = {
+        var data1 = {
             nif : document.getElementById("nif").value,
             email : document.getElementById("email").value,
             palavra_passe : document.getElementById("password").value,
@@ -268,26 +268,137 @@ function signInOwner(){
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify(data),
+            body: JSON.stringify(data1),
             })
             .then((data) => {
-            console.log('Success:', data);
+                window.location.replace("http://127.0.0.1:5500/HTML/frontPageOwner.html");
             })
             .catch((error) => {
             console.error('Error:', error);
         });
-
-        window.location.replace("http://127.0.0.1:5500/HTML/frontPageOwner.html");
-
 
     } else {
         document.getElementById("error").textContent = "Palavras passes não coincidem";
     }
 }
 
+//---------------------------------LOG OUT-----------------------------------
+
+function logOutUser(){
+
+    var data1 ={
+        nothing: ""
+    }
+
+    fetch('http://127.0.0.1:8080/api/cliente/login', {  ////////// qual o url
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data1),
+    })
+
+    .catch((error) => {
+        console.error('Error:', error);
+    })
+
+    window.location.replace("http://127.0.0.1:5500/HTML/index.html")
+
+}
+
+function logOutOwner(){
+
+    var data1 = {
+        nothing: ""
+    }
+
+    fetch('http://127.0.0.1:8080/api/cliente/login', {  ////////// qual o url
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data1),
+    })
+
+    .catch((error) => {
+        console.error('Error:', error);
+    })
+
+    window.location.replace("http://127.0.0.1:5500/HTML/index.html")
+
+}
 
 
+//---------------------------------CHANGE PERFIL-----------------------------
 
+
+function savePerfil(){
+
+    let password1 = document.getElementById("password").value;
+    let password2 = document.getElementById("secondPassword").value;
+    
+    if( password1 === password2 ){
+
+        var data1 = {
+            nome_utilizador : document.getElementById("username"),
+            palavra_passe : document.getElementById("password"),
+            num_telemovel : document.getElementById("phoneNumber")
+        }
+
+
+        fetch('http://127.0.0.1:8080/api/cliente/alterar_dados', {   ////////// qual o url
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+            })
+            .then((data) => {
+                document.getElementById("response1").textContent = "Alterações aceites";
+            })
+            .catch((error) => {
+            console.error('Error:', error);
+        });
+
+    } else {
+        document.getElementById("response1").textContent = "Palavras passes não coincidem";
+    }
+
+}
+
+
+function saveFilter(){
+
+    let km = document.getElementById("kilometers").value;
+
+    if(km > 0){
+        var data1 = {
+            filter : km
+        }
+
+
+        fetch('http://127.0.0.1:8080/api/cliente/alterar_dados', {   ////////// qual o url
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+            })
+            .then((data) => {
+                document.getElementById("response2").textContent = "Alteração aceite";
+            })
+            .catch((error) => {
+            console.error('Error:', error);
+        });
+    }
+    else{
+        document.getElementById("response2").textContent = "Distância inserida não permitida";
+    }
+
+
+}
+
+//------------------------------------rest-----------------------------------
 
 function addRestaurante(){
 
@@ -349,6 +460,9 @@ function getRestaurant(){
 
 
 
+
+
+
 //------------------------------CHANGE WINDOW METHODS-----------------------
 
 function toLogInUser(){
@@ -402,4 +516,9 @@ function toBookings(){
 
 function toRestaurante(){
     window.location.replace("http://127.0.0.1:5500/HTML/restaurante.html")
+}
+
+function toAddRest(){
+    window.location.replace("http://127.0.0.1:5500/HTML/addRestaurante.html")
+
 }
